937e3dc335dd8c7867f9614e548fa53ccf7cc8d6
awawilow <apetkau@upb.de>
2019-04-15 15:21:23 +0200

    new version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { FileService } from './services';
 import { Faksimile } from './types/faksimile';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 
 
 @Component({
@@ -24,6 +25,23 @@ export class AppComponent{
     this.faksimiles = this.fileService.getFaksimiles();
     console.log("close...");
     console.log("a.." + data.title);
+  }
+  onTaskDrop(event: any) {
+
+    var myElement = event.source.element.nativeElement;
+    myElement.style.opacity = 0.5;
+    myElement.style.zIndex = 999;
+    //myElement.parentNode.appendChild(myElement);
+    var children = myElement.parentNode.childNodes;
+    for (let i = 0; i < children.length; i++) {
+      if (children[i].tagName == "DIV" && children[i] != myElement)
+      {
+        children[i].style.opacity = 1.0;
+        console.log(children[i].tagName);
+      }
+     
+    }
+   
   }
 
   title = 'ng7-pre';
