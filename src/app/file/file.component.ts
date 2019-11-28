8101dc268f5f7d3beae810085eaff82ae6c93f95
Anastastasia <apetkau@upb.de>
2019-11-28 14:17:22 +0100

    d3 and numeric libraries

--- src/app/file/file.component.ts
+++ src/app/file/file.component.ts
@@ -8,7 +8,6 @@ import Map from 'ol/Map.js';
 import View from 'ol/View.js';
 import Projection from 'ol/proj/Projection.js';
 import { Image as ImageLayer} from 'ol/layer.js';
-import { getCenter } from 'ol/extent.js';
 import Static from 'ol/source/ImageStatic.js'
 import { MapFaksimile } from '../types/mapfaksimile';
 import { ZoomSlider } from 'ol/control';
@@ -28,20 +27,31 @@ import { Vector as VectorSource } from 'ol/source';
 import FocusMap from 'ol-ext/interaction/FocusMap';
 import Transform from 'ol-ext/interaction/Transform';
 import Delete from 'ol-ext/interaction/Delete';
+import Swipe from 'ol-ext/control/Swipe';
 import { shiftKeyOnly } from 'ol/events/condition';
+
+import SourceState from 'ol/source/State';
+import { fromLonLat, toLonLat } from 'ol/proj';
+import { getWidth, getCenter } from 'ol/extent';
+import { Layer, Tile as TileLayer } from 'ol/layer';
 
 
 import { Stroke, Fill, Circle, Style } from 'ol/style';
 import GeometryType from 'ol/geom/GeometryType';
 
-
+import * as d3 from "d3";
+import { Overlay } from 'ol';
+import OverlayPositioning from 'ol/OverlayPositioning';
 
 
 declare var MarvinImage: any;
 declare var Marvin: any;
 
+declare var solve: any;
+
 declare var pdfjsLib: any;
 
+//declare var d3: any;
 
 @Injectable({ providedIn: 'root' })
 
@@ -72,20 +82,25 @@ export class FileComponent {
   form;
   pdfDoc;
 
+  host: any;
+  svg: any;
+
   constructor(private fileService: FileService, private mapService: MapService) {
     
   }
 
   wrapFunction = function (fn, context, params) {
-  return function () {
-    fn.apply(context, params);
-  };
+    return function () {
+      fn.apply(context, params);
+    };
   }
+
   fun_blue_and_white;
   fun_red_and_white;
   fun_black_and_white;
   fun_edge_detection;
   fun_remove_background;
+
 
   blue_and_white = function (faksimile: Faksimile) {
     var imageProcessed = new MarvinImage();
@@ -203,7 +218,7 @@ export class FileComponent {
     this.generateMap(data, num);
 }
 
-
+  
  
 
   generateMinPreview(faksimile: Faksimile) {
@@ -282,12 +297,14 @@ export class FileComponent {
    
   }
 
+  vector;
+
   generateMap(faksimile: Faksimile, num: number) {
     if (num == null)
       num = 1;
 
     var imageProcessed = new MarvinImage();
-    var containt: any = faksimile.pages[num-1].actualcontain;
+    var containt: any = faksimile.pages[num - 1].actualcontain;
 
     var extent = [0, 0, containt.canvas.width, containt.canvas.height];
     var projection = new Projection({
@@ -304,7 +321,7 @@ export class FileComponent {
     if (mapfk != null) {
       var map = mapfk.map;
       var layers = mapfk.map.getLayers();
-     // mapfk.map.removeLayer(layers.getArray()[0]);
+      // mapfk.map.removeLayer(layers.getArray()[0]);
 
       for (var i = layers.getArray().length - 1; i >= 0; --i) {
         mapfk.map.removeLayer(layers.getArray()[i]);
@@ -329,15 +346,15 @@ export class FileComponent {
           projection: projection,
           center: getCenter(extent),
           zoom: 3,
-   
+
         })
       });
-     
+
       var legend = new Legend({
         title: faksimile.title,
         collapsed: true
       });
-     
+
 
       var barbottomright = new Bar();
       map.addControl(barbottomright);
@@ -347,26 +364,27 @@ export class FileComponent {
 
       var mapf = new MapFaksimile(map, faksimile);
       this.mapService.addMap(mapf);
-      
+
     }
 
 
-    
+
+
     var layer: any =
       new ImageLayer({
         source: new Static({
           url: url,
-         // imageSize: [containt.canvas.width, containt.canvas.height],
+          // imageSize: [containt.canvas.width, containt.canvas.height],
           projection: projection,
           imageExtent: extent
         })
       });
-    
+
     map.addLayer(layer);
 
     var source = new VectorSource({ wrapX: false });
 
-    var vector = new VectorLayer({
+    this.vector = new VectorLayer({
       source: source,
       style: new Style({
         fill: new Fill({
@@ -386,12 +404,13 @@ export class FileComponent {
     });
 
 
-    map.addLayer(vector);
+    map.addLayer(this.vector);
+
 
     var zoomslider = new ZoomSlider();
     map.addControl(zoomslider);
     faksimile.size = [containt.canvas.width, containt.canvas.height];
-  //  map.setSize([containt.canvas.width, containt.canvas.height]);
+    //  map.setSize([containt.canvas.width, containt.canvas.height]);
     //map.updateSize();
     // Main control bar
 
@@ -401,7 +420,7 @@ export class FileComponent {
 
     var zoommfactor = new TextButton(
       {
-        html: ' Zoom: <input id= "zoom-div'+ faksimile.ID + '" class= "fa fa-lg" style="width: 3em;" min="1" max="30" step="0.01" type="number" value="' + map.getView().getZoom() + '"> ',
+        html: ' Zoom: <input id= "zoom-div' + faksimile.ID + '" class= "fa fa-lg" style="width: 3em;" min="1" max="30" step="0.01" type="number" value="' + map.getView().getZoom() + '"> ',
         title: "Zoom factor",
         handleClick: function (event: any) {
           self.bindZoomInputs(event, faksimile);
@@ -415,10 +434,10 @@ export class FileComponent {
     mainbartopright.setPosition("top-right");
 
     // Control
-   
+
 
     var self = this;
-    
+
     var hold = new Toggle(
       {
         html: '<i class="fa fa-check-circle"></i>',
@@ -444,7 +463,7 @@ export class FileComponent {
       });
 
     mainbartopright.addControl(hold);
-  
+
     var blackwhite = new Button(
       {
         html: '<i class="fa fa-adjust"></i>',
@@ -505,17 +524,17 @@ export class FileComponent {
       });
     mainbartopright.addControl(removebackground);
 
-   /* var movetofront = new Button(
-      {
-        html: '<i class="fa fa-clone"></i>',
-        title: 'Move To Front',
-        handleClick: function () {
-          var canvas: any = document.getElementById('card-div' + faksimile.ID);
-          canvas.style.zIndex = ++self.inc_index;
-        
-        }
-      });
-    mainbartopright.addControl(movetofront);*/
+    /* var movetofront = new Button(
+       {
+         html: '<i class="fa fa-clone"></i>',
+         title: 'Move To Front',
+         handleClick: function () {
+           var canvas: any = document.getElementById('card-div' + faksimile.ID);
+           canvas.style.zIndex = ++self.inc_index;
+         
+         }
+       });
+     mainbartopright.addControl(movetofront);*/
     var geometryFunction = createBox();
     var value = 'Circle';
     var draw = new Draw({
@@ -533,12 +552,28 @@ export class FileComponent {
     var delete_interaction = new Delete();
 
     var modify_interaction = new Modify({
-      source: vector.getSource(),
+      source: this.vector.getSource(),
       // insertVertexCondition: function(){ return false; }
     });
-    
 
-    var drawBox = new Toggle(
+    var ele = document.createElement('div');
+    var pos: OverlayPositioning = OverlayPositioning.TOP_LEFT;
+    let crop = new Overlay({
+      element: ele,
+    });
+
+    var ondrawend = function (e) {
+     // console.log(e.feature.getGeometry().getCoordinates());
+      var coord: [] = e.feature.getGeometry().getCoordinates()[0].slice(0, -1);
+      var elem = crop.getElement();
+      crop.setPosition(coord);
+      drawCrop(elem, coord);
+     
+
+      console.log(coord, crop.getPosition(), crop.getPositioning());
+    };
+
+   /* var drawBox = new Toggle(
       {
         html: '<i class="fas fa-square"></i>',
         title: 'Draw Box',
@@ -552,6 +587,7 @@ export class FileComponent {
             map.removeInteraction(transform_interaction);
             map.removeInteraction(delete_interaction);
             map.removeInteraction(focusmap);
+            draw.on('drawend', ondrawend);
             map.addInteraction(draw);
           }
 
@@ -560,9 +596,9 @@ export class FileComponent {
           }
         }
       });
-    mainbartopright.addControl(drawBox);
+    mainbartopright.addControl(drawBox);*/
 
-    var select = new Toggle(
+   /* var select = new Toggle(
       {
         html: '<i class="fas fa-expand-arrows-alt"></i>',
         title: 'Select Box',
@@ -586,11 +622,139 @@ export class FileComponent {
           }
         }
       });
-    mainbartopright.addControl(select);
+    mainbartopright.addControl(select);*/
+
+
+    function drawCrop(ele, coords) {
+
+      var margin = { top: 50, right: 280, bottom: 50, left: 280 },
+        width = 600,
+        height = 600;
+       
+     // var sourcePoints = [[0, 0], [width, 0], [width, height], [0, height]];
+      //var targetPoints = [[0, 0], [width, 0], [width, height], [0, height]];
 
 
 
-    var modify = new Toggle(
+      var sourcePoints = coords;
+      var targetPoints = coords;
+
+      var svg = d3.select(ele).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var line = svg.selectAll(".line")
+        .data(d3.range(0, width + 1, 40).map(function (x) { return [[x, 0], [x, height]]; })
+          .concat(d3.range(0, height + 1, 40).map(function (y) { return [[0, y], [width, y]]; })))
+        .enter().append("path")
+        .attr("class", "line line--x");
+
+      var handle = svg.selectAll(".handle")
+        .data(targetPoints)
+        .enter().append("circle")
+        .attr("class", "handle")
+        .attr("transform", function (d) { return "translate(" + d + ")"; })
+        .attr("r", 7)
+        .call(d3.drag()
+          .subject(function (d) { return { x: d[0], y: d[1] }; })
+          .on("drag", dragged));
+
+      d3.selectAll("button")
+        .datum(function (d) { return JSON.parse((this as any).getAttribute("data-targets")); })
+        .on("click", clicked)
+        .call(transformed);
+
+      function clicked(d) {
+        d3.transition()
+          .duration(750)
+          .tween("points", function () {
+            var i = d3.interpolate(targetPoints, d);
+            return function (t) {
+              handle.data(targetPoints = i(t)).attr("transform", function (d) { return "translate(" + d + ")"; });
+              transformed();
+            };
+          });
+      }
+
+      function dragged(d) {
+        d3.select(this).attr("transform", "translate(" + (d[0] = d3.event.x) + "," + (d[1] = d3.event.y) + ")");
+        transformed();
+      }
+
+      function transformed() {
+        for (var a = [], b = [], i = 0, n = sourcePoints.length; i < n; ++i) {
+          var s = sourcePoints[i], t = targetPoints[i];
+          a.push([s[0], s[1], 1, 0, 0, 0, -s[0] * t[0], -s[1] * t[0]]), b.push(t[0]);
+          a.push([0, 0, 0, s[0], s[1], 1, -s[0] * t[1], -s[1] * t[1]]), b.push(t[1]);
+        }
+
+        var X = solve(a, b, true), matrix = [
+          X[0], X[3], 0, X[6],
+          X[1], X[4], 0, X[7],
+          0, 0, 1, 0,
+          X[2], X[5], 0, 1
+        ].map(function (x) {
+          return x;
+        });
+
+        line.attr("d", function (d) {
+          return "M" + project(matrix, d[0]) + "L" + project(matrix, d[1]);
+        });
+      }
+
+      // Given a 4x4 perspective transformation matrix, and a 2D point (a 2x1 vector),
+      // applies the transformation matrix by converting the point to homogeneous
+      // coordinates at z=0, post-multiplying, and then applying a perspective divide.
+      function project(matrix, point) {
+        point = multiply(matrix, [point[0], point[1], 0, 1]);
+        return [point[0] / point[3], point[1] / point[3]];
+      }
+
+      // Post-multiply a 4x4 matrix in column-major order by a 4x1 column vector:
+      // [ m0 m4 m8  m12 ]   [ v0 ]   [ x ]
+      // [ m1 m5 m9  m13 ] * [ v1 ] = [ y ]
+      // [ m2 m6 m10 m14 ]   [ v2 ]   [ z ]
+      // [ m3 m7 m11 m15 ]   [ v3 ]   [ w ]
+      function multiply(matrix, vector) {
+        return [
+          matrix[0] * vector[0] + matrix[4] * vector[1] + matrix[8] * vector[2] + matrix[12] * vector[3],
+          matrix[1] * vector[0] + matrix[5] * vector[1] + matrix[9] * vector[2] + matrix[13] * vector[3],
+          matrix[2] * vector[0] + matrix[6] * vector[1] + matrix[10] * vector[2] + matrix[14] * vector[3],
+          matrix[3] * vector[0] + matrix[7] * vector[1] + matrix[11] * vector[2] + matrix[15] * vector[3]
+        ];
+      }
+
+    }
+  
+
+
+   /* var select2 = new Toggle(
+      {
+        html: '<i class="fas fa-expand-arrows-alt"></i>',
+        title: 'Select 2 Box',
+        active: false,
+        onToggle: function (active) {
+
+          if (active) {
+            map.addOverlay(crop);
+            //drawCrop(ele);
+            //map.addInteraction(selectClick);
+
+            //map.addInteraction(draw);
+          }
+
+          else {
+         
+          }
+        }
+      });
+    mainbartopright.addControl(select2);*/
+
+
+
+  /*  var modify = new Toggle(
       {
         html: '<i class="fas fa-vector-square"></i>',
         title: 'Modify',
@@ -612,9 +776,9 @@ export class FileComponent {
           }
         }
       });
-    mainbartopright.addControl(modify);
+    mainbartopright.addControl(modify);*/
    
-    var remove = new Toggle(
+    /*var remove = new Toggle(
       {
         html: '<i class="fas fa-trash"></i>',
         title: 'Remove Box',
@@ -638,7 +802,32 @@ export class FileComponent {
           }
         }
       });
-    mainbartopright.addControl(remove);
+    mainbartopright.addControl(remove);*/
+
+   /* var swipe = new Toggle(
+      {
+        html: '<i class="fas fa-sliders-h"></i>',
+        title: 'Swipe',
+        active: false,
+        onToggle: function (active) {
+          if (active) {
+            drawBox.setActive(false);
+            modify.setActive(false);
+            select.setActive(false);
+            map.removeInteraction(draw);
+            map.removeInteraction(modify);
+            map.removeInteraction(transform_interaction);
+
+            var ctrl = new Swipe();
+            map.addControl(ctrl);
+          }
+
+          else {
+           // map.removeInteraction(delete_interaction);
+          }
+        }
+      });
+    mainbartopright.addControl(swipe);*/
 
     var undo = new Button(
       {
@@ -664,7 +853,6 @@ export class FileComponent {
         title: "Magnify",
         active: false,
         onToggle: function (active) {
-         var allLayers = null; 
          var ov = new Magnify(
            {
              layers: [layer],
